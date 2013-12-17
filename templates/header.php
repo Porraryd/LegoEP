8a85c452365cb5eec4b1690708b7a7a4ee236a5b
Johan Nordin <johanordin@gmail.com>
2013-12-17 19:17:05 +0100

    small layout and CSS fix

--- templates/header.php
+++ templates/header.php
@@ -2,18 +2,16 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
-		<title>LiTHehack är bäst! </title>
+		<title>Projekt EP!</title>
 
 		<link rel="stylesheet" href="css/style.css" type="text/css" />
 	</head>
 	<body>
 		<div id="container">
 			<header class="wrapper">
-				<h1>LiTHehack är bäst!</h1>
+				<h1>Legodatabas!</h1>
 
 				<nav>
 					<a href="index.php">Hem</a>
-					<a href="bilder.php">Bilder</a>
-					<a href="kod.zip">Koden</a>
 				</nav>
 			</header>
