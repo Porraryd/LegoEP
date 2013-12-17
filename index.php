e00cbff91054c4b56bc7f72d11cbb0485431f9a3
Johan Nordin <johanordin@gmail.com>
2013-12-17 23:10:21 +0100

    array added

--- index.php
+++ index.php
@@ -3,29 +3,47 @@ include "templates/header.php";
 include "templates/form.php";
 ?>
 			<main class="wrapper">
-
-
 <?php
-	include 'php/db_connect.php';
+	include 'templates/db_connect.php';
 
+	//connect till databasen
 	connect('lego');
 
+	//indata från form
 	$search = $_POST['search']; 
 	$type   = $_POST['type'];
 
-	echo $type;
+	echo $type . ' ';
+	//koppla val med tabell
+	$keys = array('SetID' => 'inventory',
+				  'Setname' => 'sets',
+			  	  'PartID' => 'parts',
+			  	  'Colorname' => 'colors');
 
-	$x = query('SELECT CatID from categories');
+	//Leta reda på rätt tabell
+	foreach ($keys as $key => $value) {
+		if ($type == $key){
+			$table = $value;
+			echo $table . '<br>';
+		}
+	}
+
+	$x = query("SELECT * FROM `$table`");
 	//$x = query("SELECT * 
 	//			FROM  `colors` 
 	//			WHERE Colortype =  '$search'");
 
-
 	echo mysql_num_rows($x);
 	var_dump(mysql_fetch_array($x));
 
+	echo '<br>' . $table . '<br>';
+	echo '<br>' . $type . '<br>';
 	while ( $rest = mysql_fetch_array($x) ) {
-		echo $rest['CatID'] . '<br>';
+		foreach ($rest as $key => $value) {
+			echo $value . '<br>';
+		}
+
+//echo $rest['$table'] .'<br>';
 //		echo $rest['ColorID'] . '<br>'.
 //			 $rest['Colortype'] . '<br>' . 
 //			 $rest['Colorname'] . '<br>';
