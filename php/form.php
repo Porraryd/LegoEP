d9a41f27b3328b85d900259c4f036bcf5d5d743a
Johan Nordin <johanordin@gmail.com>
2013-12-18 20:51:48 +0100

    added default to radio buttons

--- php/form.php
+++ php/form.php
@@ -5,9 +5,9 @@
 						
 						<input type="text" name="search"> 
 						<input type="submit" value="Submit"><br>
-						<input type="radio" name="type" value="SetID">SetID
-						<input type="radio" name="type" value="Setname">Setname
-						<input type="radio" name="type" value="PartID">PartID
-						<input type="radio" name="type" value="Colorname">Color
+						<input type="radio" name="type" value="SetID" autocomplete="off" checked='checked'>SetID
+						<input type="radio" name="type" value="Setname" autocomplete="off">Setname
+						<input type="radio" name="type" value="PartID" autocomplete="off">PartID
+						<input type="radio" name="type" value="Colorname" autocomplete="off">Color
 				</form>
 			</div>
