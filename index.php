89f07819152613ef0cf9c53682bed0fea95201a4
unknown <johno855@ITN-TP5021-01.ad.liu.se>
2013-12-18 14:17:56 +0100

    Första grund med fungerande frågor som visas

--- index.php
+++ index.php
@@ -1,15 +1,71 @@
 <?php
 include "templates/header.php";
+include "templates/form.php";
 ?>
 			<main class="wrapper">
-				<div class="row cf">
-					<div class="onethird">Hej!</div>
-					<div class="onethird">Hej!</div>
-					<div class="onethird">Hej!</div>
+<?php
+	include 'templates/db_connect.php';
+
+	//connect till databasen
+	connect('lego');
+
+	//indata från form
+	$search = $_POST['search']; 
+	$type   = $_POST['type'];
+
+	echo $type . ' ';
+	//koppla val med tabell
+	$keys = array('SetID' => 'inventory',
+				  'Setname' => 'sets',
+			  	  'PartID' => 'parts',
+			  	  'Colorname' => 'colors');
+
+	//Leta reda på rätt tabell
+	foreach ($keys as $key => $value) {
+		if  ($type == $key){
+			$table = $value;
+			echo $table . '<br>';
+		}
+	}
+	//Skickar frågan till connect_db
+	$x = query("SELECT * FROM `$table`");
+
+
+	echo mysql_num_rows($x);
+	var_dump(mysql_fetch_array($x));
+
+	echo '<br>';
+
+	echo '<br>' . $table . '<br>';
+	echo '<br>' . $type . '<br><br>';
+/*	while ( $rest = mysql_fetch_assoc($x) ) {
+
+		foreach ($rest as $key => $value) {
+			
+		echo $key . ' ';
+		}
+	}*/
+	echo '<br>' . $table . '<br>';
+	while ( $rest = mysql_fetch_row($x) ) {
+
+		echo '<br>';
+		foreach ($rest as $key => $value) {
+			//echo $key . ' ';
+			echo $value . ' ';
+		}
+		echo '<br>';
+	}
+
+//echo $rest['$table'] .'<br>';
+//		echo $rest['ColorID'] . '<br>'.
+//			 $rest['Colortype'] . '<br>' . 
+//			 $rest['Colorname'] . '<br>';
+	
+	//mysql_free_result();
+
+?>
 				</div>
-				<div class="row cf">
-					<div class="twothirds">Hej!</div>
-					<div class="onethird">Hej!</div>
+					<div class="row cf">
 				</div>
 			</main>
 <?php
---
+++ templates/form.php
@@ -0,0 +1,13 @@
+				
+			<div class="wrapper">
+				<div class="row cf">
+					<form action="index.php" method="post"> 
+						
+						<input type="text" name="search"> 
+						<input type="submit" value="Submit"><br>
+						<input type="radio" name="type" value="SetID">SetID
+						<input type="radio" name="type" value="Setname">Setname
+						<input type="radio" name="type" value="PartID">PartID
+						<input type="radio" name="type" value="Colorname">Color
+				</form>
+			</div>
