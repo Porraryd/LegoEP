f767a90d878a286c59cacf67d27bf9ca060c7cf7
Pontus <ponor358@student.liu.se>
2014-01-15 15:09:49 +0100

    Load Image tar med minifigs

--- php/load_image.php
+++ php/load_image.php
@@ -1,49 +1,61 @@
 <?php
+function load_image($value, $colorID)
+{
+    $pic = query("SELECT * 
+                FROM  `images` 
+                WHERE itemID='$value'
+                AND colorID='$colorID'
+                ");
 
-        function load_image($value, $colorID){
-        	
-                        $pic = query("SELECT * 
-                                  FROM  `images` 
-                            WHERE itemID='$value'
-                            AND colorID='$colorID'
-                            ");
-
-                $pics = mysql_fetch_row($pic);
-
-
-                    if ($pics[0] == 'S'){
-                        //check for small set images
-                        if($pics[3] == 1){
-                                $url = 'http://webstaff.itn.liu.se/~stegu76/img.bricklink.com' . '/' . $pics[0] . '/' . $pics[1] . '.gif';
-                        }else if($pics[4] == 1){
-                                        $url = 'http://webstaff.itn.liu.se/~stegu76/img.bricklink.com' . '/' . $pics[0] . '/' . $pics[1] . '.jpg';
-                                }else{
-                                        $url = "images/lego_small.png";
-                                }
-                        }else{
-                                //check for small part images
-                        if($pics[3] == 1){
-                                $url = 'http://webstaff.itn.liu.se/~stegu76/img.bricklink.com' . '/' . $pics[0] . '/' . $pics[2] . '/' . $pics[1] . '.gif';
-                        }else if($pics[4] == 1){
-                                        $url = 'http://webstaff.itn.liu.se/~stegu76/img.bricklink.com' . '/' . $pics[0] . '/' . $pics[2] . '/' . $pics[1] . '.jpg';
-                                }else{
-                                        $url = "images/lego_small.png";
-                                }
-                        }
-                                //check for large images
-                                if($pics[5] == 1){
-                                $url_l = 'http://webstaff.itn.liu.se/~stegu76/img.bricklink.com' . '/' . $pics[0] . 'L/' . $pics[1] . '.gif';
-                        }else if($pics[6] == 1){
-                                        $url_l = 'http://webstaff.itn.liu.se/~stegu76/img.bricklink.com' . '/' . $pics[0] . 'L/' . $pics[1] . '.jpg';
-                                }else{
-                                        $url_l = "images/lego_big.png";
-                                }
-                        
+    $pics = mysql_fetch_row($pic);
 
 
-                        return array($url, $url_l);
+    if ($pics[0] == 'P')
+    {
+        //check for small part images
+        if($pics[3] == 1){
+            $url = 'http://webstaff.itn.liu.se/~stegu76/img.bricklink.com' . '/' . $pics[0] . '/' . $pics[2] . '/' . $pics[1] . '.gif';
         }
+        else if($pics[4] == 1)
+        {
+            $url = 'http://webstaff.itn.liu.se/~stegu76/img.bricklink.com' . '/' . $pics[0] . '/' . $pics[2] . '/' . $pics[1] . '.jpg';
+        }
+        else
+        {
+            $url = "images/lego_small.png";
+        }
+    }
+    else
+    {
+            //check for small set images
+        if($pics[3] == 1)
+        {
+            $url = 'http://webstaff.itn.liu.se/~stegu76/img.bricklink.com' . '/' . $pics[0] . '/' . $pics[1] . '.gif';
+        }
+        else if($pics[4] == 1)
+        {
+            $url = 'http://webstaff.itn.liu.se/~stegu76/img.bricklink.com' . '/' . $pics[0] . '/' . $pics[1] . '.jpg';
+        }
+        else
+        {
+            $url = "images/lego_small.png";
+        }
+    }
+                
+    //check for large images
+    if($pics[5] == 1)
+    {
+        $url_l = 'http://webstaff.itn.liu.se/~stegu76/img.bricklink.com' . '/' . $pics[0] . 'L/' . $pics[1] . '.gif';
+    }
+    else if($pics[6] == 1)
+    {
+        $url_l = 'http://webstaff.itn.liu.se/~stegu76/img.bricklink.com' . '/' . $pics[0] . 'L/' . $pics[1] . '.jpg';
+    }
+    else
+    {
+        $url_l = "images/lego_big.png";
+    }
 
-
-
+    return array($url, $url_l);
+}
 ?>
